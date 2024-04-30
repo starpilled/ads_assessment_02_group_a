6de9ec87a685d8a31fc94435c3994db7b322b6aa
starpilled <5383774@gmail.com>
2024-04-30 11:15:39 +0200

    assessment (tomintz) sorry for cluttered code :(

--- main.py
+++ main.py
@@ -3,11 +3,36 @@ Simple Calculator
 """
 # Provide your solution here
 
+def calculate(a: int, b: int, operator):
+    if operator == "+":
+        print(a+b)
+    elif operator == "-":
+        print(a-b)
+    elif operator == "*":
+        print(a*b)
+    elif operator == "/":
+        if a == 0:
+            print("Division by 0 is not allowed.")
+        elif b == 0:
+            print("Division by 0 is not allowed.")
+        else:
+            print(a/b)
+    else:
+        print("Invalid operator.")
+
+calculate(0, 2, "/")
+
 
 """
 Reverse Word
 """
 # Provide your solution here
+def reverse_word(word):
+    length = len(word)
+    reverse = word[-1] + word[-2] + word[-3] + word [-4] + word[-5] +
+    print(str.capitalize(reverse))
+
+reverse_word("hELLo")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
